f082e30ee84a8dbd909fe6bdcf1e4390dc52423c
Hannah Angle <hannahcangle@gmail.com>
2020-09-15 03:02:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! IM SUFFERING. SEND HELP. Or not I dont want to inonvenience you. Im just being dramatic..."
+    "Welcome to your app!!!! IM SUFFERING. SEND HELP. Or not I dont want to inonvenience you. Im just being dramatic... But like if you do want to send help that would be great too."
   end
 
 end
